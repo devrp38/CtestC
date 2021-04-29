3defeae28b0b3f596112bc58992087abf7f54e6a
Renjith Philip <74170096+devrp38@users.noreply.github.com>
2021-04-29 18:34:14 +0530

    Created c.h
    Created C

---
+++ c.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+      int l,b;
+      printf("Enter Length:");
+      scanf("%d",&l);
+      printf("Enter Breadth:");
+      scanf("%d",&b);
+      printf("Area of Rectangle: %d",l/b);
+}
